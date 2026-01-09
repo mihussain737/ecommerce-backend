a3af38aab0c3b64bb8754bcf7ef406480ecd4dd3
Md Imam Hussain <mihussain737@gmail.com>
2026-01-09 21:36:17 +0530

    stripe service added

--- src/main/java/com/ecommerce/sb_ecom/service/StripeService.java
+++ src/main/java/com/ecommerce/sb_ecom/service/StripeService.java
@@ -1,4 +1,8 @@
 package com.ecommerce.sb_ecom.service;
+import com.ecommerce.sb_ecom.payload.StripePaymentDto;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface StripeService {
+    PaymentIntent paymentIntent(StripePaymentDto stripePaymentDto) throws StripeException;
 }
--- src/main/java/com/ecommerce/sb_ecom/service/impl/StripeServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/impl/StripeServiceImpl.java
@@ -1,4 +1,42 @@
 package com.ecommerce.sb_ecom.service.impl;
+import com.ecommerce.sb_ecom.payload.StripePaymentDto;
+import com.ecommerce.sb_ecom.service.StripeService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-public class StripeServiceImpl {
+@Service
+@Transactional
+public class StripeServiceImpl implements StripeService {
+
+    @Value("${STRIPE_SECRET_KEY}")
+    private String stripeApiKey;
+
+    @PostConstruct
+    public void init(){
+        Stripe.apiKey=stripeApiKey;
+    }
+
+    @Override
+    public PaymentIntent paymentIntent(StripePaymentDto stripePaymentDto) throws StripeException {
+
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(stripePaymentDto.getAmount()) // amount in cents
+                        .setCurrency(stripePaymentDto.getCurrency())
+                        .setAutomaticPaymentMethods(
+                                PaymentIntentCreateParams.AutomaticPaymentMethods
+                                        .builder()
+                                        .setEnabled(true)
+                                        .build()
+                        )
+                        .build();
+        PaymentIntent paymentIntent= PaymentIntent.create(params);
+        return paymentIntent;
+    }
 }
